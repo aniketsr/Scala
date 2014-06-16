83a30ec127d56164047a8a9a2d8a8c2086523627
Aniket Survase <aniketsr@thoughtworks.com>
2014-06-16 16:00:16 +0530

    Basic scala

---
+++ SayHi.scala
@@ -0,0 +1,6 @@
+def sayHi(Hi : String){
+	var Hello = Hi;
+	Hello = "Hello";
+	println(Hello);
+}
+sayHi("Hi");
---
+++ StripMargin.scala
@@ -0,0 +1,5 @@
+def print(){
+	println("""|Hello World.
+			 |How are you?""".stripMargin);
+}
+print();
